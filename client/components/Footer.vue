e5dcf24c31d507b924f1c97149d27c61ec67e863
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-13 19:15:08 +0800

    New commom footer component

---
+++ client/components/Footer.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+footer.footer
+  section.footer__source-section
+    .footer__title Source
+    a.link(href="https://github.com/silveryiris/silvery-vue-boilerplate")
+      IconMarkGithub.svg-icon.svg-icon--large.footer__icon
+</template>
+<style lang="stylus">
+.footer
+  display grid
+  align-items center
+  padding-bottom 1rem
+  grid-template-columns repeat(5, 1fr)
+
+  &__title
+    margin-bottom 1rem
+    text-transform uppercase
+    font-weight bold
+
+  &__source-section
+    justify-self center
+    grid-column 3
+
+  &__icon
+    margin 0 1rem
+    fill var(--color-main-text)
+</style>
+<script>
+import IconMarkGithub from "@primer/octicons/build/svg/mark-github.svg"
+
+export default {
+  components: { IconMarkGithub }
+}
+</script>
